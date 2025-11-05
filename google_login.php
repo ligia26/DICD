18f88e33e5ee30f806f2c653a167f9bec8a5d225
Ziyad Shoeky <ziyad@datainnovation.io>
2025-11-05 15:13:05 +0000

    google_login.php edited online with Bitbucket

--- google_login.php
+++ google_login.php
@@ -5,21 +5,17 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once 'vendor/autoload.php';
-require_once 'includes/config.php';
 
 session_start();
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+$client = new Google_Client();
+$client->setClientId($_ENV['GOOGLE_CLIENT_ID']);
+$client->setClientSecret($_ENV['GOOGLE_CLIENT_SECRET']);
+$client->setRedirectUri($_ENV['GOOGLE_REDIRECT_URI']);
+$client->addScope("email");
+$client->addScope("profile");
 
-try {
-    $client = new Google_Client();
-    $client->setClientId(Config::getRequired('GOOGLE_CLIENT_ID'));
-    $client->setClientSecret(Config::getRequired('GOOGLE_CLIENT_SECRET'));
-    $client->setRedirectUri(Config::getRequired('GOOGLE_REDIRECT_URI'));
-    $client->addScope("email");
-    $client->addScope("profile");
-
-    $auth_url = $client->createAuthUrl();
-    header('Location: ' . filter_var($auth_url, FILTER_SANITIZE_URL));
-} catch (Exception $e) {
-    die("Configuration error: " . $e->getMessage());
-}
+$auth_url = $client->createAuthUrl();
+header('Location: ' . filter_var($auth_url, FILTER_SANITIZE_URL));
 ?>
